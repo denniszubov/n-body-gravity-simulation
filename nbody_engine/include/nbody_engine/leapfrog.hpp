bb6b89134ee60b34a758defbc546faed7d2ee4ac
Dennis Zubov <denniszubov365@gmail.com>
2026-02-08 10:57:31 -0500

    fix leapfrog calc

--- nbody_engine/include/nbody_engine/leapfrog.hpp
+++ nbody_engine/include/nbody_engine/leapfrog.hpp
@@ -5,6 +5,6 @@
 
 namespace nbody_engine {
 
-void leapfrog_step(State& state, nbody_engine::real dt, nbody_engine::real G, nbody_engine::real eps);
+void leapfrog_step(State& state, real dt, real G, real eps);
 
 }
--- nbody_engine/src/leapfrog.cpp
+++ nbody_engine/src/leapfrog.cpp
@@ -1,15 +1,16 @@
 #include <nbody_engine/state.hpp>
 #include <nbody_engine/gravity.hpp>
+#include <nbody_engine/leapfrog.hpp>
 #include <cstddef>
 
 
 namespace nbody_engine {
 
-void leapfrog_step(State& state, nbody_engine::real dt, nbody_engine::real G, nbody_engine::real eps) {
+void leapfrog_step(State& state, real dt, real G, real eps) {
     // using kick-drift-kick for stability.
     // assumes acceleration has been calculated for current state.
 
-    for (std::size_t i; i < state.size(); ++i) {
+    for (std::size_t i = 0; i < state.size(); ++i) {
         // kick
         state.velocity[i].x = state.velocity[i].x + (state.acceleration[i].x * 0.5 * dt);
         state.velocity[i].y = state.velocity[i].y + (state.acceleration[i].y * 0.5 * dt);
@@ -17,13 +18,15 @@ void leapfrog_step(State& state, nbody_engine::real dt, nbody_engine::real G, nb
         // drift
         state.position[i].x = state.position[i].x + (state.velocity[i].x * dt);
         state.position[i].y = state.position[i].y + (state.velocity[i].y * dt);
+    }
 
-        // recompute acceleration
-        nbody_engine::compute_acceleration(state, G, eps);
+    // recompute acceleration
+    compute_acceleration(state, G, eps);
 
+    for (std::size_t i = 0; i < state.size(); ++i) {
         // kick
-        state.velocity[i].x = state.velocity[i].x + (state.acceleration[i].x + 0.5 * dt);
-        state.velocity[i].y = state.velocity[i].y + (state.acceleration[i].y + 0.5 * dt);
+        state.velocity[i].x = state.velocity[i].x + (state.acceleration[i].x * 0.5 * dt);
+        state.velocity[i].y = state.velocity[i].y + (state.acceleration[i].y * 0.5 * dt);
     }
 }
 
